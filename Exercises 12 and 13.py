02e6dfe4639cac15fc5a731b40bb8f12bc80fee4
shaye875 <sm0548540902@gmail.com>
2025-06-17 19:25:02 +0300

    Add files via upload

---
+++ Exercises 12 and 13.py
@@ -0,0 +1,18 @@
+#c12
+# arr = ["a","c","d","s"]
+# for i in range(len(arr)):
+#     print(arr[i]*2)
+
+#c12.2
+# for i in range(len(arr)-1):
+#     print(arr[i]+arr[i+1])
+#13
+p = [1,4,7,6,9,34]
+for i in p:
+    if i % 2 == 0:
+        print(int(i/2),end=" ")
+    else:
+        print(i,end=" ")
+
+
+
---
+++ practic functions 2.py
@@ -0,0 +1,30 @@
+#c1
+# def print_hello():
+#     print("hello world")
+#c2
+# def sum_and_multi(x,y):
+#     print("the sum is",x+y)
+#     print("the multi is",x*y)
+# sum_and_multi(3,5)
+#c3
+# def list_sum_and_multi(x,y):
+#     listi = []
+#     listi.append(x+y)
+#     listi.append(x*y)
+#     print(listi)
+# list_sum_and_multi(7,5)
+#c4
+# def even_or_not(x):
+#     if x % 2 == 0:
+#         print(True)
+#     else:
+#         print(False)
+# even_or_not(4)
+#c5
+# def str(x,y,t):
+#     return  x+y+t
+# print(str("asi","kobi","avi"))
+#c6
+# def reverse(str):
+#     return str[::-1]
+# print(reverse("shahaf"))
